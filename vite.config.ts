685cdf56e12be0759dbe34fb04d61f7639e9f0bc
Anderson Marlon <anderson18.marlon@gmail.com>
2024-05-01 20:00:45 -0300

    Added: Million - 01-05 20:00

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import million from "million/compiler";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+ 
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    million.vite({
+      auto: {
+        threshold: 0.05,
+        skip: ["useBadHook", /badVariable/g],
+      },
+    }),
+    react(),
+  ],
+});
